cfd9c9d4f671e673da7274f37d52921fd9f104dc
Younes SEFIANI <tokyotweener21@gmail.com>
2024-05-22 17:09:08 +0200

    Quête 03.2 - validateUser - Mèthode de validation de la saisie de l'email de l'utilsateur via le module (Joi).

--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -1,18 +1,26 @@
+const Joi = require("joi");
+
+const userSchema = Joi.object({
+  email: Joi.string().email().max(255).required(),
+  firstname: Joi.string().max(255).required(),
+  lastname: Joi.string().max(255).required(),
+  city: Joi.string().max(255).required(),
+  language: Joi.string().max(255).required(),
+});
+
 const validateUser = (req, res, next) => {
-    const { email } = req.body;
-    const errors = [];
-  
-    const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
-  
-    if (!emailRegex.test(email)) {
-      errors.push({ field: 'email', message: 'Invalid email' });
-    }
-  
-    if (errors.length) {
-      res.status(422).json({ validationErrors: errors });
-    } else {
-      next();
-    }
-  };
+  const { firstname, lastname, email, city, language } = req.body;
+
+  const { error } = userSchema.validate(
+    { firstname, lastname, email, city, language },
+    { abortEarly: false }
+  );
+
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+};
 
   module.exports = validateUser;
