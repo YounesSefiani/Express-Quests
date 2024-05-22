aba6006b42e7fe9c3928287c2fcc5dcd8f10ee75
Younes SEFIANI <tokyotweener21@gmail.com>
2024-05-22 16:55:27 +0200

    Quête 03.2 - validateUser - Email : Mèthode de validation manuelle de la saisie de l'email de l'utilsateur.

--- src/app.js
+++ src/app.js
@@ -9,13 +9,14 @@ app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const validateMovie = require("./middlewares/validateMovie");
+const validateUser = require("./middlewares/validateUser");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUsersById);
 app.post("/api/movies", validateMovie, movieControllers.postMovie);
-app.post("/api/users", movieControllers.postUsers);
+app.post("/api/users", validateUser, movieControllers.postUsers);
 app.put("/api/movies/:id", movieControllers.updateMovie);
 app.put("/api/users/:id", movieControllers.updateUsers);
 
