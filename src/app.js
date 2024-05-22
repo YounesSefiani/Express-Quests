5e6df51d0a476c7fb3bb89e2c45fa203d87725af
Younes SEFIANI <tokyotweener21@gmail.com>
2024-05-22 16:44:38 +0200

    Quête 03.2 - validateMovie - Champs manquants : Mèthode 1 de validation manuelle de la saisie de l'utilsateur.

--- src/app.js
+++ src/app.js
@@ -8,12 +8,13 @@ const app = express();
 app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
+const validateMovie = require("./middlewares/validateMovie");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUsersById);
-app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
 app.post("/api/users", movieControllers.postUsers);
 app.put("/api/movies/:id", movieControllers.updateMovie);
 app.put("/api/users/:id", movieControllers.updateUsers);
