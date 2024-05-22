7d391fc2cfdeb961da2e02e38cd9435aedf24fef
Younes SEFIANI <tokyotweener21@gmail.com>
2024-05-22 17:31:40 +0200

    Quête 03.2 - validateUser - Mèthode de validation de la saisie de l'email de l'utilsateur via le module (express-validator).

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const port = process.env.APP_PORT
+const port = process.env.APP_PORT;
 const express = require("express");
 
 const app = express();
@@ -9,14 +9,30 @@ app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const validateMovie = require("./middlewares/validateMovie");
-const validateUser = require("./middlewares/validateUser");
+// const validateUser = require("./middlewares/validateUser");
+const { body, validationResult } = require('express-validator');
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUsersById);
 app.post("/api/movies", validateMovie, movieControllers.postMovie);
-app.post("/api/users", validateUser, movieControllers.postUsers);
+app.post(
+  "/api/users",
+  body("firstname").isLength({ min: 1, max: 255 }),
+  body("lastname").isLength({ min: 1, max: 255 }),
+  body("email").isEmail(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(422).json({ validationErrors: errors.array() });
+    } else {
+      next();
+    }
+  },
+  movieControllers.postUsers
+);
 app.put("/api/movies/:id", movieControllers.updateMovie);
 app.put("/api/users/:id", movieControllers.updateUsers);
 
